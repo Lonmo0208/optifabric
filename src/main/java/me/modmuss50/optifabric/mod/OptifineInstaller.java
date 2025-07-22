9a0f6efbd73f84d9ae1fe2aa137bdef6b896c31f
tildejustin <spacepepper3.14@gmail.com>
2025-07-21 22:26:33 -0400

    capitalize OptiFabric as well

--- src/main/java/me/modmuss50/optifabric/mod/OptifineInstaller.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifineInstaller.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 // a class used to extract the optifine jar from the installer
 public class OptifineInstaller {
     public static void extract(Path installer, Path output, Path minecraftJar) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, MalformedURLException {
-        System.out.println("Running optifine patcher");
+        System.out.println("Running OptiFine patcher");
         @SuppressWarnings("resource") ClassLoader classLoader = new URLClassLoader(new URL[]{installer.toUri().toURL()}, OptifineInstaller.class.getClassLoader());
         Class<?> clazz = classLoader.loadClass("optifine.Patcher");
         Method method = clazz.getDeclaredMethod("process", File.class, File.class, File.class);
